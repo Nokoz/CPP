0d295bde2644e212f27c8cb0ee68874b6afd2cdb
Guillaume Vardakis <gvardaki@shi-r11-p5.s19.be>
2024-01-23 12:54:54 +0100

    cpp00 ex00 OK

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gvardaki <gvardaki@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:28:59 by gvardaki          #+#    #+#             */
-/*   Updated: 2024/01/23 12:00:35 by gvardaki         ###   ########.fr       */
+/*   Updated: 2024/01/23 12:52:49 by gvardaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ int	main(int ac, char **av)
 {
 	if (ac == 1)
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	if (ac > 1)
+	{
+		for (int j = 1; j < ac; j++)
+		{
+			for (int i = 0; av[j][i] != '\0'; i++)
+				cout << (char) toupper(av[j][i]);
+			cout << " ";
+		}
+		cout << endl;
+	}
 	return (0);
-}
-
-int	ft_toupper(int c)
-{
-	if (c > 96 && c < 123)
-		return (c - 32);
-	else
-		return (c);
 }
