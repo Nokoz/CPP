746546e9eb7b4114dc6950e14b5ccd466376c302
Guillaume Vardakis <gvardaki@shi-r11-p5.s19.be>
2024-01-24 08:23:12 +0100

    no using namespace

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,27 +6,25 @@
 /*   By: gvardaki <gvardaki@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:28:59 by gvardaki          #+#    #+#             */
-/*   Updated: 2024/01/23 12:52:49 by gvardaki         ###   ########.fr       */
+/*   Updated: 2024/01/24 08:22:14 by gvardaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using namespace std;
-
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	if (ac > 1)
 	{
 		for (int j = 1; j < ac; j++)
 		{
 			for (int i = 0; av[j][i] != '\0'; i++)
-				cout << (char) toupper(av[j][i]);
-			cout << " ";
+				std::cout << (char) toupper(av[j][i]);
+			std::cout << " ";
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 	return (0);
 }
